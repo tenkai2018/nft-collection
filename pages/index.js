c883172192a703fafa02f54f872730c595efd304
tenkai2018 <44020259+tenkai2018@users.noreply.github.com>
2022-07-08 10:49:37 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -376,7 +376,7 @@ export default function Home() {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src="./cryptodevs/0.svg" />
+          <img className={styles.image} src="./cryptodevs/NFT.jpg" />
         </div>
       </div>
 
